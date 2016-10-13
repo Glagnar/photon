d0301f21196fc5cacf7c35f9e7a20f37040a1f9b
Thomas Gilbert <thomas.gilbert@alexandra.dk>
2016-10-13 11:36:39 +0200

    Some examples and libraries

---
+++ Program.ino
@@ -0,0 +1,14 @@
+
+// Define stuff here
+int i = 0
+
+// This is called once, on startup
+void setup() {
+  // Do pin assignment or other stuff here
+}
+
+// This loop is called continuously
+void loop(void) {
+  // So, a busy wait - is that all you can think of ?
+  delay(1000);
+}
